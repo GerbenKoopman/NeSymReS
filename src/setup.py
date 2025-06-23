4c9268f2c87849e1af2406d0dc3f12ed323229db
Gerben Koopman <gerbennkoopman@gmail.com>
2025-06-23 17:24:24 +0200

    Added torch and pl to the setup file

--- src/setup.py
+++ src/setup.py
@@ -1,12 +1,26 @@
-import setuptools 
+import setuptools
 
 setuptools.setup(
-      version='0.1.0',
-      description='Code for reproducing the results of Neural Symbolic Regression that scales',
-      name="nesymres",
-      packages=setuptools.find_packages('.'),
-      package_dir={'': '.'},
-      install_requires=[
-          'numpy','sympy', 'pandas','click', 'tqdm','numexpr','jsons', "h5py","scipy","dataclass_dict_convert", "hydra-core==1.0.0", "ordered_set", "wandb"
-      ]
-     )
+    version="0.1.1",
+    description="Ablation and Extension study of Neural Symbolic Regression that Scales",
+    name="nesymres",
+    packages=setuptools.find_packages("."),
+    package_dir={"": "."},
+    install_requires=[
+        "numpy",
+        "sympy",
+        "pandas",
+        "click",
+        "tqdm",
+        "numexpr",
+        "jsons",
+        "h5py",
+        "scipy",
+        "dataclass_dict_convert",
+        "hydra-core==1.0.0",
+        "ordered_set",
+        "wandb",
+        "torch",
+        "pytorch_lightning",
+    ],
+)
