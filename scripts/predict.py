e48d241c461d208b6c8e05604749c470abe0e889
Cezary Klamra <cklamra@icloud.com>
2025-06-25 16:56:31 +0200

    inference loop

--- scripts/predict.py
+++ scripts/predict.py
@@ -10,27 +10,31 @@ import pandas as pd
 from tqdm import tqdm
 from functools import partial
 from collections import defaultdict
+import warnings
 
 
 from nesymres.architectures.model import Model
 from nesymres.dclasses import FitParams, NNEquation, BFGSParams
 
+warnings.filterwarnings('ignore')
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_file', '-i', type=str, default='/home/cezary/Projects/IEiAI-NeSymReS/test_set/nc.csv')
-    parser.add_argument('--output_dir', '-o', type=str, default='/home/cezary/Projects/IEiAI-NeSymReS/test_results/')
+    parser.add_argument('--input_file', '-i', type=str, default='/home/cezary/Projects/IEiAI/test_set/nc.csv')
+    parser.add_argument('--output_dir', '-o', type=str, default='/home/cezary/Projects/IEiAI/test_results/')
     
     # parser.add_argument('--model_ckpt', '-m', type=str, default='/home/cezary/Projects/IEiAI-NeSymReS/weights/10M.ckpt')
     # parser.add_argument('--model_conf', '-c', type=str, default='/home/cezary/Projects/IEiAI-NeSymReS/jupyter/10MPaper/config.yaml')
     # parser.add_argument('--eq_setting', '-e', type=str, default='/home/cezary/Projects/IEiAI-NeSymReS/jupyter/10MPaper/equation_config.json')
 
-    parser.add_argument('--model_ckpt', '-m', type=str, default='/home/cezary/Projects/IEiAI-NeSymReS/weights/100M.ckpt')
-    parser.add_argument('--model_conf', '-c', type=str, default='/home/cezary/Projects/IEiAI-NeSymReS/jupyter/100M/config.yaml')
-    parser.add_argument('--eq_setting', '-e', type=str, default='/home/cezary/Projects/IEiAI-NeSymReS/jupyter/100M/eq_setting.json')
+    parser.add_argument('--model_ckpt', '-m', type=str, default='/home/cezary/Projects/IEiAI/weights/100M.ckpt')
+    parser.add_argument('--model_conf', '-c', type=str, default='/home/cezary/Projects/IEiAI/jupyter/100M/config.yaml')
+    parser.add_argument('--eq_setting', '-e', type=str, default='/home/cezary/Projects/IEiAI/jupyter/100M/eq_setting.json')
 
-    parser.add_argument('--num_points_pred', type=int, default=500)
-    parser.add_argument('--num_points_eval', type=int, default=10000)
+    parser.add_argument('--num_equations', type=int, default=20)
+    # parser.add_argument('--num_points_pred', type=int, default=500)
+    # parser.add_argument('--num_points_eval', type=int, default=10000)
     return parser.parse_args()
 
 
@@ -46,8 +50,8 @@ def sample_points(equation, support, n_points, eq_setting, n_trials=10):
     _eq = sp.lambdify(','.join(eq_setting['total_variables']), equation, modules='numpy')
 
     trial = 0
-    # points = torch.empty(dtype=torch.float)
-    points = torch.tensor([], dtype=torch.float)
+    points = torch.empty(size=(0,), dtype=torch.float)
+    # points = torch.tensor([], dtype=torch.float)
     targets = points.clone()
     while trial < n_trials and len(points) < n_points:
         _points = torch.rand(n_points, n_variables) * (_max - _min) + _min
@@ -84,7 +88,7 @@ def predict(args):
     )
     params_fit = FitParams(
         word2id=eq_setting["word2id"],
-        id2word={int(k): v for k,v in eq_setting["id2word"].items()},
+        id2word={int(k): v for k, v in eq_setting["id2word"].items()},
         una_ops=eq_setting["una_ops"],
         bin_ops=eq_setting["bin_ops"],
         total_variables=list(eq_setting["total_variables"]),
@@ -101,30 +105,27 @@ def predict(args):
 
     fitfunc = partial(model.fitfunc, cfg_params=params_fit)
 
+    num_equations = len(data) if args.num_equations is None else \
+        min(args.num_equations, len(data))
     preds = defaultdict(list)
-
     for i, _, expr, supp, num_points in tqdm(
-        data[:10].itertuples(), total=len(data)
+        data[:num_equations].itertuples(), total=num_equations
     ):
         _supp = eval(supp)['x_1']
         X, y = sample_points(expr, _supp, num_points, eq_setting)
-        # eq_true = sp.sympify(expr)
         if X is None or y is None:
             continue
 
-        pred = fitfunc(X, y)
+        pred = fitfunc(X, y)[0]['output']
 
         preds['expr_true'].append(expr)
         preds['expr_pred'].append(pred['best_bfgs_preds'][0])
-        preds['support'].append(supp)
+        preds['support'].append(_supp)
         preds['num_points'].append(num_points)
-        print(expr)
-        print(pred['best_bfgs_preds'][0])
+        print('True:', expr)
+        print('Pred:', pred['best_bfgs_preds'][0])
 
     return pd.DataFrame(preds)
-
-def evaluate():
-    pass
 
 
 def main():
@@ -133,6 +134,7 @@ def main():
 
     preds = predict(args)
     print(preds)
+    preds.to_csv(os.path.join(args.output_dir, 'pred_nesymres.csv'))
 
 
 if __name__ == '__main__':
